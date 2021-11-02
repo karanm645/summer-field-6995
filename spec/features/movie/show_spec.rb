5b447d40506a824fa4ba415fa935bdfa64c8ea09
Karan Mehta <karanm645@gmail.com>
2021-11-02 12:54:52 -0500

    Update model & feat. spec files for order method

--- spec/features/movie/show_spec.rb
+++ spec/features/movie/show_spec.rb
@@ -7,9 +7,27 @@ RSpec.describe 'user sees one movie' do
 
       visit "/movies/#{movie1.id}" 
 
-      cexpect(page).to have_content(movie1.title)
-      cexpect(page).to have_content(movie1.creation_year)
-      cexpect(page).to have_content(movie1.genre)
+      expect(page).to have_content(movie1.title)
+      expect(page).to have_content(movie1.creation_year)
+      expect(page).to have_content(movie1.genre)
+    end 
+    it 'displays movies actors from youngest to oldest' do 
+      studio1 = Studio.create!(name: "Shotgun Production", location: "Brooklyn, NY")
+      movie1 = studio1.movies.create!(title: "Gone with the wind", creation_year: 1977, genre: "Country Western")
+      karan = Actor.create!(name: 'Karan', age: 30)
+      john = Actor.create!(name: 'John', age: 22)
+      ziggy = Actor.create!(name: 'Ziggy', age: 11)
+
+      ActorMovie.create!(movie_id: movie1.id, actor_id: karan.id)
+      ActorMovie.create!(movie_id: movie1.id, actor_id: john.id)
+      ActorMovie.create!(movie_id: movie1.id, actor_id: ziggy.id)
+
+      visit "movies/#{movie1.id}"
+
+      expect(page).to have_content((karan.name))
+      expect(page).to have_content((john.name))
+      expect(john.name).to appear_before(karan.name)
+      expect(ziggy.name).to appear_before(john.name)
     end 
   end 
 end 
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe Movie do
     it { should validate_presence_of :creation_year}
     it { should validate_presence_of :genre}
   end 
+
+  describe 'ordering the actors' do 
+    it 'has actors in order from youngest to oldest' do 
+      studio1 = Studio.create!(name: "Shotgun Production", location: "Brooklyn, NY")
+      movie1 = studio1.movies.create!(title: "Gone with the wind", creation_year: 1977, genre: "Country Western")
+      karan = Actor.create!(name: 'Karan', age: 30)
+      john = Actor.create!(name: 'John', age: 22)
+      ziggy = Actor.create!(name: 'Ziggy', age: 11)
+
+      ActorMovie.create!(movie_id: movie1.id, actor_id: karan.id)
+      ActorMovie.create!(movie_id: movie1.id, actor_id: john.id)
+      ActorMovie.create!(movie_id: movie1.id, actor_id: ziggy.id)
+
+      expect(movie1.order_actors).to eq([ziggy, john, karan])
+    end 
+  end 
 end
