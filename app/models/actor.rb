3108fc12ab6bac75143c89c7434c5760b0b7753f
Karan Mehta <karanm645@gmail.com>
2021-11-02 10:34:39 -0500

    Establish validations

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,4 @@
 class Actor < ApplicationRecord 
+  has_many :movies
+  validates_presence_of :name, :age 
 end 
--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Actor do
   describe 'relationships' do 
     it { should have_many(:movies) }
   end 
+
+  describe 'validations' do 
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :age}
+  end 
 end 
